433852507b021406e7e568b208e4b2cf4b7f035d
Balu Vymajala <vvmanyam@gmail.com>
2019-08-30 19:24:29 -0400

    multiplication formula test

--- packages/module2/runStrings.js
+++ packages/module2/runStrings.js
@@ -3,4 +3,4 @@
 const { concatthem } = require("./strings");
 const { multiply } = require("module1/math");
 console.log(concatthem("xyz", "abc"));
-console.log(multiply(10, 20));
+console.log(multiply(20, 20));
