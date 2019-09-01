71461ee15c62a3509d29189a7c63854088784cea
Balu Vymajala <vvmanyam@gmail.com>
2019-09-01 11:56:06 -0400

    changed only module2

--- packages/module2/runStrings.js
+++ packages/module2/runStrings.js
@@ -4,3 +4,4 @@ const { concatthem } = require("./strings");
 const { multiply } = require("module1/math");
 console.log(concatthem("xyz", "abc"));
 console.log(multiply(30, 40));
+console.log(multiply(30, 40));
